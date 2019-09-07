268ddd0a21fc1686c04c0f2a107a266f41a23ad0
Miguel Galindo <mgalindob@gmail.com>
2019-09-07 12:37:38 -0500

    imported from snack

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, StyleSheet } from 'react-native';
+import TicTacToe from './src/components/TicTacToe';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+const App = () => (
+  <View style={styles.container}>
+    <TicTacToe />
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+export default App;
---
+++ src/components/Board.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import Box from './Box';
+
+const Board = props => {
+  const renderRow = rowNum => {
+    const columns = [1, 2, 3];
+    const { curPlayer, values, onPlayerMove } = props;
+    return columns.map(colNum => {
+      const index = colNum + 3 * rowNum;
+      return (
+        <Box
+          key={index}
+          index={index}
+          value={values[index - 1]}
+          curPlayer={curPlayer}
+          onPlayerMove={onPlayerMove}
+        />
+      );
+    });
+  };
+
+  const rows = [0, 1, 2];
+  const { curPlayer } = props;
+  return (
+    <View style={styles.boardContainer}>
+      {rows.map(rowNum => {
+        return (
+          <View key={rowNum.toString()} style={styles.row}>
+            {renderRow(rowNum)}
+          </View>
+        );
+      })}
+      <Text style={styles.playerText}>Next Player: {curPlayer}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  boardContainer: {
+    flex: 0.8,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  playerText: {
+    marginTop: 25,
+    fontSize: 30,
+    color: '#4A8305',
+  },
+  row: {
+    flexDirection: 'row',
+  },
+});
+
+export default Board;
