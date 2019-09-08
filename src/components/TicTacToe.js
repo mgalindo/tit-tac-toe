07e39cf01ec91e605d4f360626cf8f21149d247e
Miguel Galindo <mgalindob@gmail.com>
2019-09-08 10:13:19 -0500

    Added gameHelper util

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -1,23 +1,7 @@
 import * as React from "react";
 import { Alert, Button, View, StyleSheet } from "react-native";
 import Board from "./Board";
-
-/*
-Assumes the board tiles are numbered like this
-0 1 2
-3 4 5
-6 7 8
-*/
-const winCombinations = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-];
+import { getWinner } from "../util/gameHelpers";
 
 export default class TicTacToe extends React.Component {
   getPlayer = moveIndex => {
@@ -46,23 +30,9 @@ export default class TicTacToe extends React.Component {
     );
   };
 
-  getWinner = values => {
-    for (let winComb of winCombinations) {
-      const [index1, index2, index3] = winComb;
-      if (
-        values[index1].trim() !== "" &&
-        values[index1] === values[index2] &&
-        values[index1] === values[index3]
-      ) {
-        return values[index1];
-      }
-    }
-    return null;
-  };
-
   processNewState = () => {
     const { curMove, values } = this.state;
-    const winner = this.getWinner(values);
+    const winner = getWinner(values);
     if (winner) {
       Alert.alert("The winner is: " + winner, "", [
         { text: "OK", onPress: this.restartGame }
