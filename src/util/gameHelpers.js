dd41cb27e4d52bf806728c943c618115b745343c
Miguel Galindo <mgalindob@gmail.com>
2019-09-08 10:46:46 -0500

    helper unit test

--- src/util/gameHelpers.js
+++ src/util/gameHelpers.js
@@ -15,18 +15,18 @@ const winCombinations = [
   [2, 4, 6]
 ];
 
-  const getWinner = values => {
-    for (let winComb of winCombinations) {
-      const [index1, index2, index3] = winComb;
-      if (
-        values[index1].trim() !== "" &&
-        values[index1] === values[index2] &&
-        values[index1] === values[index3]
-      ) {
-        return values[index1];
-      }
+const getWinner = values => {
+  for (let winComb of winCombinations) {
+    const [index1, index2, index3] = winComb;
+    if (
+      values[index1].trim() !== "" &&
+      values[index1] === values[index2] &&
+      values[index1] === values[index3]
+    ) {
+      return values[index1];
     }
-    return null;
-  };
+  }
+  return null;
+};
 
-  export { getWinner };
+export { getWinner };
